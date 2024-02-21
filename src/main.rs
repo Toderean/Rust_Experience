b136ae87662fe326bb2628e3be7b9ba9ad615361
Toderean <todereanrazvan@yahoo.com>
2024-02-21 18:55:12 +0200

    final chapters

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,93 @@
-// #[derive(Debug, PartialEq, Copy, Clone)]
-// enum ShirtColor {
-//     Red,
-//     Blue,
+// use std::thread;
+// use std::time::Duration;
+// fn main(){
+//     // let handle = thread::spawn(|| {
+//     //     for i in 1..10{
+//     //         println!("spawned thread number  = {}", i);
+//     //         thread::sleep(Duration::from_secs(1));
+//     //     }
+//     // });
+//     // handle.join().unwrap();
+//     // for i in 1..5{
+//     //     println!("main thread number = {}",i);
+//     //     thread::sleep(Duration::from_secs(1));
+//     // }
+
+    
+//     let v = vec![1, 2, 3];
+
+//     let handle = thread::spawn(move || {
+//         println!("Here's a vector: {:?}", v);
+//     });
+
+//     handle.join().unwrap();
 // }
 
-// struct Inventory {
-//     shirts: Vec<ShirtColor>,
-// }
-
-// impl Inventory {
-//     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
-//         user_preference.unwrap_or_else(|| self.most_stocked())
-//     }
-
-//     fn most_stocked(&self) -> ShirtColor {
-//         let mut num_red = 0;
-//         let mut num_blue = 0;
-
-//         for color in &self.shirts {
-//             match color {
-//                 ShirtColor::Red => num_red += 1,
-//                 ShirtColor::Blue => num_blue += 1,
-//             }
-//         }
-//         if num_red > num_blue {
-//             ShirtColor::Red
-//         } else {
-//             ShirtColor::Blue
-//         }
-//     }
-// }
-
-// fn main() {
-//     let store = Inventory {
-//         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
-//     };
-
-//     let user_pref1 = Some(ShirtColor::Red);
-//     let giveaway1 = store.giveaway(user_pref1);
-//     println!(
-//         "The user with preference {:?} gets {:?}",
-//         user_pref1, giveaway1
-//     );
-
-//     let user_pref2 = None;
-//     let giveaway2 = store.giveaway(user_pref2);
-//     println!(
-//         "The user with preference {:?} gets {:?}",
-//         user_pref2, giveaway2
-//     );
-// }
-
+use std::sync::{mpsc, Arc};
+use std::thread;
+use std::time::Duration;
+use std::sync::Mutex;
+use std::rc::Rc;
 
 fn main(){
-    let a = "abc   cdf  ";
-    println!("{},orice",a);
+    // let (tx,rx)  = mpsc::channel();
+    // let tx2 = tx.clone();
+
+    // thread::spawn(move || {
+    //     let msgs  = vec![
+    //         String::from("Hello"),
+    //         String::from("brothers"),
+    //         String::from("and"),
+    //         String::from("sisters"),
+    //     ];
+    //     for val in msgs{
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    // thread::spawn(move || {
+    //     let msgs  = vec![
+    //         String::from("inca"),
+    //         String::from("cateva"),
+    //         String::from("mesage"),
+    //         String::from("bai"),
+    //     ];
+    //     for val in msgs{
+    //         tx2.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    // // let received = rx.recv().unwrap();
+    // for received in rx{
+    //     println!("{}",received);
+    // }
+
+    let m = Rc::new(Mutex::new(5));
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m :{:?}",m);
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handless=  vec![];
+
+    for _ in  0..10 {
+        let counter2 = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter2.lock().unwrap();
+            *num += 1;
+        });
+        handless.push(handle);
+    }
+
+    for handle in handless{
+        handle.join().unwrap();
+    }
+
+    println!("Result {} ",*counter.lock().unwrap());
 
 }
